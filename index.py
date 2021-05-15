6d66d827bb076dee00c4b23b0cafd192aa741c9e
ReeLee <84224291+ReeLeeSama@users.noreply.github.com>
2021-05-15 14:58:27 +0500

    Update index.py

--- index.py
+++ index.py
@@ -14,56 +14,7 @@ client.remove_command('help')
 @client.event
 async def on_ready():
     print("Bot is online")
-
-@client.command()
-async def ping(ctx):
-    embed=discord.Embed(title="MDB Ping",description=f"My ping is {round(client.latency * 1000)}ms ",color=discord.Colour.blurple())
-    await ctx.reply(embed=embed)
-
-@client.command()
-async def uptime(ctx):
-    delta_uptime = datetime.utcnow() - client.launch_time
-    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    embed=discord.Embed(title="MDB Uptime",description=f"{days}d, {hours}h, {minutes}m, {seconds}s",color=discord.Colour.blurple())
-    await ctx.reply(embed=embed)
-
-@client.command()
-async def stats(ctx):
-    guilds = client.guilds
-    members = 0
-
-    for guild in guilds:
-        gmembers = guild.members
-        num = len(gmembers)
-        members += num
-        
-    delta_uptime = datetime.utcnow() - client.launch_time
-    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    embed = discord.Embed(title="MDb Stats",description=f"Client Status  :  \n```Servers  ::  {str(len(client.guilds))}\nUsers  ::  {members}\nPing  ::  {round(client.latency * 1000)}ms\nUptime  ::  {days}d, {hours}h, {minutes}m, {seconds}s```",color=discord.Colour.blurple())
-    await ctx.send(embed=embed)
-
-
-@client.command()
-async def remindme(ctx,time: time_str.convert,*,message=None):
-    if message == None:
-        message ="Something"
-    emoji = client.get_emoji(842736272631660598)
-    await ctx.reply(f"{emoji} Reminder set ")
-    await asyncio.sleep(time.total_seconds())
-    mention = f"{ctx.author.mention}"
-    embed=discord.Embed(title="Reminder",description=f"{time} ago you asked about to remind you about ``{message}``",color=discord.Colour.blurple())
-    await ctx.send(mention,embed=embed)
-
-'''
-@client.command()
-async def emoji(ctx):
-    emoji = client.get_emoji(842736272631660598)
-    await ctx.reply(emoji)
-'''
+    
 @client.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx,member:discord.Member,reason=None):
